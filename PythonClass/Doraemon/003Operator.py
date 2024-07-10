c16545379561abae8314b703921a60fb0b1156c9
Aadesh <aadeshlokhande11@gmail.com>
2024-07-10 10:41:36 +0530

    10 / 07 / 2024

---
+++ PythonClass/Doraemon/003Operator.py
@@ -0,0 +1,76 @@
+# Operator
+
+# arithmatic 
+# unary 
+# a = 10 
+# print(a)
+# print(-a)
+# print(+a)
+
+# Binary
+# a = 10 
+# b = 20 
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a % b)
+
+# Relationship
+# a = 10 
+# b = 20
+# print(a < b) # ----> True 
+# print(a > b) # ----> False  
+# print(a <= b) # ----> True 
+# print(a >= b) # ----> False 
+# print(a == b) # ----> False 
+# print(a != b) # ----> True
+
+# logical
+# and - ziddi
+# dhokla        idli     feel
+#  0            0           0 
+#  0            1           0 
+#  1            0           0 
+#  1            1           1    
+
+# or - greeb manacha manus
+# dhokla        idli     feel
+#   0           0           0 
+#   0           1           1 
+#   1           0           1 
+#   1           1           1 
+
+# not - ulte khopdi 
+# 1 ----> 0
+# 0 ----> 1
+
+# not true ---> false 
+# not false ----> true 
+
+
+# assignment
+
+# a = 10 
+# b = a 
+
+# a = 10
+# print(a)
+# # a = 12 
+# # a = a + 2
+# a += 2 
+# print(a)
+# a -= 2 
+# print(a)
+# a *= 2 
+# print(a)
+# a /= 2 
+# print(a)
+# a %= 2 
+# print(a)
+
+# ===============================
+# a = int(input("enter a number = "))
+# b = int(input("enter a number = "))
+# c = a + b 
+# print(a,"+",b,"=",c)
