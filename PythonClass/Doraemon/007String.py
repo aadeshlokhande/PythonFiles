a92ed72be9cf673cd6db78eccaa9ba106c1553e8
Aadesh <aadeshlokhande11@gmail.com>
2024-07-19 17:44:05 +0530

    19 / 07 / 2024

---
+++ PythonClass/Doraemon/007String.py
@@ -0,0 +1,31 @@
+# String
+
+# a = 'abc'
+# b = "abc"
+# c = '''abc'''
+# d = """abc"""
+
+# a = "this is my mom's mobile"
+# b = 'modi said "mitro".' 
+
+# a = """asda"""
+# print(a)
+
+# slicing
+
+# a = "aadesh"
+# print(type(a))
+
+# a = "kitne aadmi the"
+# print(a)
+# print(a[2])
+# print(a[-3])
+# print(a[10:])
+# print(a[:10])
+# print(a[2:7])
+# print(a[-7:-2])
+# print(a[:-7])
+# print(a[-7:])
+# print(a[1:7:1])
+# print(a[7:1:-1])
+# print(a[::-2])
