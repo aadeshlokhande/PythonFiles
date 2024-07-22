89af5556e98972621d97ee0ef2afbdf89d786a23
Aadesh <aadeshlokhande11@gmail.com>
2024-07-22 17:20:26 +0530

    22 / 07 / 2024

--- PythonClass/Doraemon/007String.py
+++ PythonClass/Doraemon/007String.py
@@ -29,3 +29,60 @@
 # print(a[1:7:1])
 # print(a[7:1:-1])
 # print(a[::-2])
+
+
+# ****************** Methods ******************
+
+
+# str = "If you can cry for me... Then I can die for you"
+
+# print(str)
+# print(str.upper())     
+# print(str.lower())
+# print(str.capitalize())
+# print(str.title())
+# print(str.swapcase())
+
+# print(str.split())
+# print(str.strip())
+# print(str.replace("can", "could"))
+# print(str.center(70,"*"))
+# print(str.count("can"))
+
+# print(str.find("can"))
+# print(str.index("can"))
+
+# print(str.isprintable())
+# print("123".isdecimal())
+# print("123".isdigit())
+# print("asd123".isalnum())
+# print("aasd".isalpha())
+
+
+
+# a = "abcde"
+# print("-".join(a))
+
+# a = ["my","name", "is", "aadesh"]
+# print(" ".join(a))
+
+# a = "my name is aadesh"
+# print(a.split())
+
+
+# ****************** format ******************
+
+# "my name is aadesh and my age is 80 years old"
+
+# name = input("enter your name = ")
+# age = int(input("enter your age = "))
+# a = "my name is {} and my age is {} years old".format(name,age )
+# a = "my name is {1} and my age is {0} years old".format(name,age )
+# a = f"my name is {name} and my age is {age} years old"
+# print(a)
+
+
+# a = "my name is "+name+"."
+# print(a)
+
+# print("aadesh\n"*10)
