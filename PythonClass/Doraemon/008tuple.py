5f4074a5e1abfcd31103f975aa1c50fd310cc636
Aadesh <aadeshlokhande11@gmail.com>
2024-07-25 10:09:14 +0530

    25 / 07 / 2024

---
+++ PythonClass/Doraemon/008tuple.py
@@ -0,0 +1,24 @@
+# Tuple - non mutable - ()
+
+# a = (10,20,30)
+
+# print(a)
+# print(type(a))
+
+
+# a = ("aadesh", 3.14, 564)
+
+# a = 23,54,67,98
+# print(a)
+
+
+# a = 10 
+# b = 20 
+# a,b = b,a
+# print(a,b)
+
+# a = ("aadesh", 3.14, 564)
+# print(a[0])
+
+# print(a.count(564))
+# print(a.index(564))
---
+++ PythonClass/Doraemon/009List.py
@@ -0,0 +1,56 @@
+# list ---> mutable ----> []
+
+# a = [12,43,76]
+# print(a)
+# print(type(a))
+
+
+# itemList = ["tamater", "aalu", "gobi"]
+# print(itemList)
+# itemList.append("bhindi")
+# print(itemList)
+# itemList.insert(1,"pyaj")
+# print(itemList)
+# itemList.remove("aalu")
+# itemList.pop(2)
+# print(itemList)
+
+# list2 = ["mirchi", "nimbu", "adrak"]
+
+# itemList.append(list2)
+# itemList.extend(list2)
+# print(itemList)
+
+# itemList.sort()
+# print(itemList)
+
+# print(dir(itemList))
+# itemList.clear()
+# b = itemList.copy()
+# a = itemList.count("aalu")
+# print(a)
+
+# ===============================
+
+# a = [12,32,54,6,56]
+# a = (12,32,43)
+# a = "Aadesh"
+# print(len(a))
+# a = []
+# a = list()
+# a = "aadesh"
+# a = (12,543,76)
+# b = list(a)
+# b.append("Aadesh")
+# a = tuple(b)
+# print(a,b)
+# a = tuple()
+# a = ()
+
+
+# =====================================
+
+# a = [12,32,[21,("aadesh",87,76),32,43],54]
+# # print(len(a))
+# print(a)
+# print(a[2][1][0][3])
