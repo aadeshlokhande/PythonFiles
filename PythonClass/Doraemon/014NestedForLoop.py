f327add7cf8bcd19c1fb20d4ba2c5e58ff57b962
Aadesh <aadeshlokhande11@gmail.com>
2024-08-02 19:28:55 +0530

    02 / 08 / 2024

--- PythonClass/Doraemon/014NestedForLoop.py
+++ PythonClass/Doraemon/014NestedForLoop.py
@@ -94,7 +94,6 @@
 #         print(a, end = " ")
 #     print()
 
-
 # =====================================================
 
 # *
@@ -145,24 +144,12 @@
 # 4 3 2 1
 # 5 4 3 2 1
 
-
-# 1
-# 2 1
-# 3 2 1
-# 4 3 2 1
-# 5 4 3 2 1
-
 # 5
 # 5 4
 # 5 4 3
 # 5 4 3 2
 # 5 4 3 2 1
 
-# 5
-# 5 4
-# 5 4 3
-# 5 4 3 2
-# 5 4 3 2 1
 
 # 5
 # 4 5
@@ -170,11 +157,7 @@
 # 2 3 4 5
 # 1 2 3 4 5
 
-
-
 # 1 - 10 tables
-
-
 
 # 1 - 100 counting
 # 1 2 3 ......10
@@ -185,8 +168,72 @@
 # .
 # 91 92 93......100
 
+# ====================================================
+
+# for i in range(1,11):
+#     for j in range(1,11):
+#         print(i*j, end="\t")
+#     print()
+
+
+# a = 0
+# for i in range(1,11):
+#     for j in range(1,11):
+#         a += 1
+#         print(a, end = "\t")
+#     print()
 
 
 
+# ==============================================
+
+# ASCII 
+# A - 65 + 26
+# a - 90
 
 
+# number system 
+# binary - 2 - 0,1
+# octal - 8 - 0...7
+# dec - 10 - 0...9
+# hexa dec - 16 - 0...15, 0..9, a..f 
+
+# A ---> 65 
+
+# a ---> 97
+# A ----> 65 
+
+
+# a = ord("a")
+# print(a)
+
+# a = chr(97)
+# print(a)
+
+
+# a 
+# b b 
+# c c c 
+# d d d d 
+# e e e e e 
+# a = ord('a')
+# for i in range(1,6):
+#     for j in range(i):
+#         print(chr(a), end= " ")
+#     a += 1
+#     print()
+
+
+
+# a 
+# a b 
+# a b c 
+# a b c d 
+# a b c d e 
+
+# for i in range(1,6):
+#     a = ord('a')
+#     for j in range(i):
+#         print(chr(a), end= " ")
+#         a += 1
+#     print()
