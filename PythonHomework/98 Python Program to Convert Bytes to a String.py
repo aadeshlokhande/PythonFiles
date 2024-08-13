0424030cac1cce5f8f3a18d65111005044004dbc
Aadesh <aadeshlokhande11@gmail.com>
2024-08-13 11:53:39 +0530

    13 / 08 / 2024

--- PythonHomework/98 Python Program to Convert Bytes to a String.py
+++ PythonHomework/98 Python Program to Convert Bytes to a String.py
@@ -1,2 +1,15 @@
 
-print(b'Easy \xE2\x9C\x85'.decode("utf-8"))
+# https://www.compart.com/en/unicode/html
+
+# print(b'Easy \xE2\x9C\x85'.decode("utf-8"))
+
+
+# =<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=<:>=
+
+
+# file = open("secretCode.py","w")
+
+# a = b'Easy \xE2\x92\xB7'.decode("utf-8")
+# print(a)
+print("█"*10)
+
