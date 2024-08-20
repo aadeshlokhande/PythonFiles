ff8505c6a921f6ff4c7d5332ba8b5bcc61415b93
Aadesh <aadeshlokhande11@gmail.com>
2024-08-20 20:09:29 +0530

    20 / 08 / 2024

---
+++ PythonClass/Doraemon/016c.py
@@ -0,0 +1,41 @@
+# automation
+# import pyautogui as pg 
+# from time import sleep
+
+
+# sleep(2)
+# for i in range(10):
+#     pg.typewrite("hello guys :-)",0.1)
+#     pg.press("enter")
+# pg.hotkey("ctrl","a")
+
+# Keyboard
+# type
+# shortcuts 
+# press 
+
+# mouse
+# location
+# a = pg.position()
+# print(a)
+
+# click 
+# pg.click(200,400)
+
+
+# move 
+# pg.moveTo(200,400,2)
+
+# drag 
+# pg.dragTo(300,100)
+
+
+# scroll 
+# pg.scroll(-600)
+
+# double click 
+# pg.doubleClick()
+
+# right click 
+# pg.rightClick()
+
