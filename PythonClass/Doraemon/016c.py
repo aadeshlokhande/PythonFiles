e3991eff5ad36f5ce4a502d753565aa67f495e25
Aadesh <aadeshlokhande11@gmail.com>
2024-08-28 19:55:33 +0530

    28 / 08 / 2024

--- PythonClass/Doraemon/016c.py
+++ PythonClass/Doraemon/016c.py
@@ -22,13 +22,11 @@
 # click 
 # pg.click(200,400)
 
-
 # move 
 # pg.moveTo(200,400,2)
 
 # drag 
 # pg.dragTo(300,100)
-
 
 # scroll 
 # pg.scroll(-600)
@@ -38,4 +36,34 @@
 
 # right click 
 # pg.rightClick()
+
+
+
+# ====================================================================
+
+
+# import pyautogui as pg 
+# from time import sleep
+
+# # a = pg.position()
+# # print(a)
+# pg.press("win")
+# sleep(1)
+# pg.typewrite("whatsapp",0.1)
+# sleep(1)
+# pg.press("enter")
+# sleep(1)
+# pg.hotkey("win", "up")
+# sleep(1)
+# pg.typewrite("sneha python june", 0.1)
+# sleep(2)
+# pg.click(417, 176)
+# sleep(2)
+
+# a = ["hello", "gm", "buddy"]
+
+# for i in a:
+#     pg.typewrite(i,0.1)
+#     sleep(3)
+#     pg.press("enter")
 
---
+++ PythonClass/Doraemon/017FileHandling.py
@@ -0,0 +1,65 @@
+# FILE HANDLING
+
+
+# read mode 
+# file = open("abc.txt","r")
+# data = file.read()
+# for i in range(5):
+#     a = file.readline()
+#     print(a)
+
+# for i in file:
+#     print(i)
+# file.close()
+# print(data)
+
+
+# write mode 
+
+# file = open("abcde.txt", "w")
+# file.write("chin tapak dam dam")
+# file.close()
+
+
+# append mode 
+
+# file = open("abcde.txt", "a")
+# file.write("chin tapak dam dam\n")
+# file.close()
+
+
+
+# ===================================
+# file = open("info.csv", "a")
+# file.write("roll, name, age\n")
+
+# a = int(input("kitna data store krna hai = "))
+
+# for i in range(a):
+#     roll = input("enter a roll = ")
+#     name = input("enter a name = ")
+#     age  = input("enter a age = ")
+
+#     file.write(f"{roll}, {name}, {age}\n")
+#     print("----------data stored------")
+
+# file.close()
+
+
+# ================================================
+
+# num = int(input("enter a number = "))
+
+# if(num==2):
+#     print("Even number")
+# elif(num==3):
+#     print("Odd number")
+# .
+# .
+# .
+# elif(num==1000):
+#     print("even number")
+# else:
+#     print("out of range ()")
+
+# ===============================================
