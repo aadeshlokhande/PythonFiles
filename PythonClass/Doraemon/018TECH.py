68c02b349678ab84d6603936421004e0f7ce0f88
Aadesh <aadeshlokhande11@gmail.com>
2024-08-30 11:37:14 +0530

    30 / 08 / 2024

---
+++ PythonClass/Doraemon/018TECH.py
@@ -0,0 +1,23 @@
+# pip install Tech-In-Seconds 
+
+# from Tech_In_Seconds import agl 
+
+# ['AllChemElements', 'Contact', 'CountAll', 'LearnExcelFormula', 'ListOfAllExcelFunction',  'barakhadi', 'chemEleInfo', 'factorial', 'fibbonacciSequence', 'isArmstrongNumber', 'isEven', 'isOdd', 'isPalindrome', 'mean', 'romanToDecimal', 'tables', 'textAnalyse']
+
+# a = agl.AllChemElements()
+# a = agl.chemEleInfo("O")
+# a = agl.CountAll("Aadesh Lokhande 123")
+# a = agl.barakhadi()
+# a = agl.ListOfAllExcelFunction()
+# a = agl.LearnExcelFormula("lookup")
+# a = agl.factorial(12)
+# a = agl.fibbonacciSequence(10)
+# a = agl.isArmstrongNumber("153")
+# a = agl.mean("beautiful")
+# a = agl.romanToDecimal("xxx")
+# a = agl.tables(1,10)
+# a = agl.textAnalyse("Aadesh Lokhande")
+# a = agl.Contact()
+# print(a)
+
+
---
+++ PythonClass/Doraemon/prettytableDemo.py
@@ -0,0 +1,29 @@
+# from prettytable import PrettyTable
+
+# table = PrettyTable(["Roll","Name","age"])
+
+# table.add_row(["101", "sneha","21"])
+# table.add_row(["102", "shruti","20"])
+# table.add_row(["103", "srusti","21"])
+# table.add_row(["104", "saniya","20"])
+
+# print(table)
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+
+# from prettytable import PrettyTable
+
+# lst = list(range(1,11))
+# table = PrettyTable(lst)
+
+# for i in range(1,11):
+#     pop = []
+#     for j in range(1,11):
+#         pop.append(i*j)
+#     table.add_row(pop)
+
+# print(table)
+
+
+
+
