173f0e3916dee8d7d6864065d4719c7a7c1834ac
Aadesh <aadeshlokhande11@gmail.com>
2024-09-12 14:27:09 +0530

    12 / 09 / 2024

---
+++ SEP Python/001.py
@@ -0,0 +1,24 @@
+# python 
+
+
+# print("hello aadesh how are you")
+# print(10+3)
+
+# print("hello....\n"*10)
+
+
+# 12 + 90 - 43 / 874 * 25
+
+# a = input("enter equ = ")
+# print(eval(a))
+
+# 130+ 
+# 3 mini 
+
+# 45 + 15 = 2 month 
+# 3,999 /- (core + automation)
+# 1000 post
+
+# udemy 
+# 2000 rs ---> free 
+
