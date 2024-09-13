3a7151354202d48cd70797d5a2b145ce93088911
Aadesh <aadeshlokhande11@gmail.com>
2024-09-13 21:10:20 +0530

    13 / 09 / 2024

---
+++ sepBatchOnline/001basic.py
@@ -0,0 +1,150 @@
+# basics
+
+# print function
+# a = print
+# a("hello")
+
+# print("chin tapak",end="")
+# print(" dam dam")
+
+# print(12,43,65,34,76, sep="-")
+# date = 13
+# month = 9
+# year = 2024
+# print(date,month,year,sep="/")
+# date/month/year
+# hh:mm:ss 
+
+# abc = open("aadesh.txt","w")
+# print("abrakadabra",file=abc)
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+# tokens
+
+# print
+# (
+#     10
+#     ,
+#     20
+#     )
+
+
+# zal ka j1.
+# print(10)
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+# ████████████████████ comments ████████████████████
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+# we are printing hello
+# print("hello")
+
+# as da sda sd asd
+
+# ASDAS DA D A SD AS D ASD 
+# ASDAS DA D A SD AS D ASD 
+# ASDAS DA D A SD AS D ASD 
+# ASDAS DA D A SD AS D ASD 
+# ASDAS DA D A SD AS D ASD 
+# ASDAS DA D A SD AS D ASD 
+# ASDAS DA D A SD AS D ASD 
+# ASDAS DA D A SD AS D ASD 
+# ASDAS DA D A SD AS D ASD
+ 
+# COMMENT (SHORTCUT) = 'CTRL' + '/'
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+# KEYWORDS - 37
+
+# import keyword
+# print(keyword.kwlist)
+# ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+# variable - 0x22434234b244af = 20
+ 
+# a = 10
+# print(a)
+# a = 988798342
+# b = 342312312
+# c = 123123123
+
+
+# glass = "pani"
+
+# vari-able
+# a = 10 
+# a = 20 
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+# datatype
+
+# int - 322,54,23,543,23, 48573259874350922222274350974325908783444750925082727904387597432509709270979074325098743250987432509837425098734509873245873254874375070970974325907597432509275903847590743259023745987324598732054
+# float - 234.654, 234.6
+# str - "23424sfdsdf4"
+
+
+# a = "hello I am a string"
+# b = 'hello I am a string'
+# c = '''hello I am a string'''
+# d = """hello I am a string"""
+
+# a = "this is my mom's mobile"
+# b = 'modi said "hello mitro".'
+# c = "asd as d asd \"a\" dsa \'sd as d sa"
+
+# print(a,b,c,sep="\n")
+# print(a)
+# print(b)
+# print(c)
+
+
+# a = """asdasdasd
+#     asd
+#     asd
+#     aa
+#     sorteda
+#     sdas
+#     defa
+#     sorteda
+#     sorteda
+#     sorted"""
+# print(a)
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+# tuple 
+# a = (12,32,54,65)
+# print(a)
+# print(type(a))
+
+# list 
+# a = [12,43,76,7]
+# print(a)
+# print(type(a))
+
+# dic 
+# a = {"name":"aadesh", "age":90}
+# print(a)
+# print(type(a))
+
+# set 
+# a = {12,32,54,12,32,76}
+# print(a)
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+# identifier
+
+# 5star, 7sister, 10cities ---> this are not allowed
+
+# _ = 1000
+# print(_ + _)
+
+# return = 10 
+
+# Return = 10 
+
+# a = 10
+# A = 20 
