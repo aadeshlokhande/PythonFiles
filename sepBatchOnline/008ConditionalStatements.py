9ee904079e4ae80c6ef75a4a9add2889cf5c10fb
Aadesh <aadeshlokhande11@gmail.com>
2024-09-21 22:11:15 +0530

    21 / 09 / 2024

---
+++ sepBatchOnline/008ConditionalStatements.py
@@ -0,0 +1,71 @@
+# conditional statements
+# if 
+
+# if (condition):
+#     code 
+#     code 
+# code 
+
+# paise = int(input("bhagwan ke nampe dede re baba = "))
+# if(paise>0):
+#     print("bhagwan tumhara bhala kre")
+
+# age = int(input("enter a age = "))
+# if(age > 18):
+#     print("adult", end=" ")
+# print("boy")
+
+# if else 
+# age = int(input("enter your age = "))
+
+# if(age > 18):
+#     print("you can drive")
+# else:
+#     print("you can't drive")
+
+# trueCode if (condtion) else flaseCode
+# num = int(input("enter a number = "))
+# print("+ve number") if (num>0) else print("-ve number")
+
+# print("+ve number") if (int(input("enter a number = "))>0) else print("-ve number")
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+# identity
+# a = [12,34,65,35]
+# b = a.copy()
+
+# print(a)
+# print(b)
+
+# if(b is not a):
+#     print("yes")
+# else:
+#     print("No")
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+# membership
+# in 
+# not in 
+# a = [12,23,34,45,56,67]
+
+# if (560 in a):
+#     print("yes")
+# else:
+#     print("No")
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩s
+# a = "kay mnte bhau zal ka j1"
+# if ('bhau' in a):
+#     print("yes")
+# else:
+#     print("No")
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+# ⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛
+# ⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛ Homework ⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛
+# ⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛
+
+# get 2 values and print greater number
+# get 3 angle and check its triangle or not 
+# get percentage and print grades using multiple if conditions
+# get year fron user and check its leap year or not 
