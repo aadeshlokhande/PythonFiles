55fca89fd25f9f3aefb16cba04401201ddc6f851
Aadesh <aadeshlokhande11@gmail.com>
2024-09-23 22:01:34 +0530

    23 / 09 / 2024

---
+++ sepBatchOnline/009nestedifelse.py
@@ -0,0 +1,106 @@
+# nested if else 
+
+# if Condition:
+#     code
+#     if Condition:
+#         code
+#     else:
+#         code
+# else:
+#     code 
+#     if Condition:
+#         code 
+#     else:
+#         code 
+
+
+# age = int(input("enter your age = "))
+
+# if (age > 0) and (age<80):
+#     if age> 18 :
+#         print("you can drive")
+#     else:
+#         print("you can't drive")
+# else:
+#     print("invalid age")
+
+# a = int(input("enter a number = "))
+# b = int(input("enter a number = "))
+# c = int(input("enter a number = "))
+
+# logic : nested if else 
+# if (a > b):
+#     if(a > c):
+#         print(a)
+#     else:
+#         print(c)
+# else:
+#     if(b > c):
+#         print(b)
+#     else:
+#         print(c)
+    
+
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+# a,b,c = input("enter a number = ").split()
+# print(a,type(a))
+# print(b,type(b))
+# print(c,type(c))
+
+
+
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+
+# if Condition:
+#     code 
+#     if Condition:
+#         code 
+#         if Condition:
+#             code 
+#         else:
+#             code 
+#     else:
+#         code 
+# else:
+#     code 
+
+
+# age = int(input("enter your age = "))
+
+# if (age > 0) and (age<80):
+#     if age > 18 :
+#         dl = input("do you have DL = ")
+#         if(dl=="yes"):
+#             print("you can drive")
+#         else :
+#             print("you can't drive")
+#     else:
+#         print("you are not eligible")
+# else:
+#     print("invalid age")
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+# goa 
+# paisa > 10000
+# fds > 5 
+# permission == yes
+
+# paise = int(input("apke budget kitna hai = "))
+# if(paise> 10000):
+#     fds = int(input("apke sath kitne fds hai = "))
+#     if(fds > 5):
+#         permission = input("parents ki permission hai ky = ")
+#         if(permission== "yes"):
+#             print("aap goa ja sakte ho")
+#         else:
+#             print("aap goa nhi ja sakte")
+#     else:
+#         print("trip ke liye kuch aur fds add kro")
+# else:
+#     print("bhag ja bhikmange")
+
+
