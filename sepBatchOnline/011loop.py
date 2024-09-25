2a51ed7f1948b33d8a796a3e6abac3d22b8e8db8
Aadesh <aadeshlokhande11@gmail.com>
2024-09-25 22:10:11 +0530

    25 / 09 / 2024

---
+++ sepBatchOnline/011loop.py
@@ -0,0 +1,73 @@
+# loops
+
+# for 
+
+# for var in collectionOfData:
+#     code 
+
+# a = "hello..."
+# a = [12,32,54,76,98,65,34]
+# a = (1,2,3,4,5,6,7)
+# a = {'name':"parth", 'age':21, 'roll':101, 'sec':'A'}
+# for i,j in a:
+#     print(i)
+#     print(a[i])
+#     print(f"key =  {i}, value = {j}")
+
+# print(a.items())
+
+# a = [10,20,30,40,50]
+# for i,j in enumerate(a):
+#     print(f"index = {i} and value = {j}")
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+# ████████████████████ range function ████████████████████
+
+# for i in range(10):
+#     print("hello...")
+
+# 0-9
+
+# for i in range(1,11,2):
+#     print(i)
+
+# for i in range(10, 0, -1):
+#     print(i)
+
+
+# a = []
+# for i in range(1,11):
+#     a.append(i)
+# print(a)
+
+# print(list(range(1,11)))
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+# for i in range(1,11,2):
+
+# a = {12,32,54......100}
+# for(int i = 1; i<11; i++)
+# for(int i = 1; i<11; i += 2)
+# i++
+# i = i + 1 
+# i += 1 
+
+
+
+# a = [12,43,12,43,65,6,45,34,65]
+# print(a)
+# a.sort(reverse=True)
+# a.sort()
+# a.reverse()
+# print(a)
+
+# DSA ---> c,c++, java, js ---> 
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+# gemini ----> 
+
+# a = "asdad"
+# a = 90
+# print(type(a))
