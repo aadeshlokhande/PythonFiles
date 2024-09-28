4506fe7e198ad09beeebecf0d1e1c6120adf7f6f
Aadesh <aadeshlokhande11@gmail.com>
2024-09-28 22:17:59 +0530

    28 / 09 / 2024

---
+++ sepBatchOnline/sep28.py
@@ -0,0 +1,105 @@
+# desktop automation
+
+# pip install pyautogui
+# pyautogui
+
+# from time import sleep
+# import pyautogui as pg
+
+# for i in range(1,11):
+#     sleep(1)
+#     print(f"nasa hacked {i*10}%")
+# print("Access Granted")
+
+# sleep(2)
+
+# keyboard 
+# typing
+# pg.typewrite("str1",0.001)
+
+
+# press 
+# pg.press("enter",5,1)
+
+# shortcut
+# pg.hotkey("ctrl", "a")
+# pg.hotkey("ctrl", "x")
+
+
+
+
+# mouse 
+# position 
+# a = pg.position()
+# print(a)
+
+
+# click 
+# pg.click()
+# pg.click(1439,652,200,0.005)
+# 0.0000036 /1 
+
+# double click
+# pg.doubleClick()
+
+# right click 
+# pg.rightClick()
+
+# drag 
+# pg.dragTo()
+# pg.drag()
+
+
+
+
+# move 
+# pg.moveTo(100,100,3)
+# pg.move(100,100,3)
+
+
+
+# scroll
+# pg.scroll(-300)
+
+# a,b = pg.locateCenterOnScreen("timer.png")
+# pg.moveTo(a,b,5)
+# pg.click()
+
+
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+# ████████████████████ sending whatsapp msg ████████████████████
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+# from time import sleep
+# import pyautogui as pg
+# a = pg.position()
+# print(a)
+
+# sleep(2*60*60)
+# pg.press("win")
+# sleep(1)
+# pg.typewrite("whatsapp",0.1)
+# pg.press("enter")
+# sleep(2)
+# pg.hotkey("win", "up")
+# sleep(1)
+# pg.typewrite("vaanya bajaj",0.1)
+# sleep(4)
+# pg.click(199,182)
+# sleep(4)
+# pg.typewrite("Happy Bday Vaanya... :-) :-) :-) ",0.1)
+# pg.press("enter")
+
+
+
+# # ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+# import webbrowser
+
+# a = ["7058232826","asdad","asdasdasd"]
+# msg = r"hello%20aadesh%20kaise%20ho%20bhai...%0Ajinda%20ho%20ya%20civd%20me%20nipt%20gye"
+# link = f"wa.me/91{a}/?text={msg}"
+
+
+# webbrowser.open(link)
+# webbrowser.open(link)
