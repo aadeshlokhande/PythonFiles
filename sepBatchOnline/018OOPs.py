af84a7137c47efcccd057c4c32c6ae92ecc7c30e
Aadesh <aadeshlokhande11@gmail.com>
2024-10-08 22:10:01 +0530

    08 / 10 / 2024

---
+++ sepBatchOnline/018OOPs.py
@@ -0,0 +1,58 @@
+# OOPs
+
+# class 
+# object 
+# inheritance
+# polymorphism
+# abstraction 
+# encapsulation
+
+# Car ---> baleno ----> 
+# mobile ----> iphone14pro -----> 3 
+
+# single 
+# multiple
+# multilevel
+# heirarchical
+
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+
+
+# class Student:
+#     "this is a student  classs"
+#     def getData(self, r, a, n):
+#         self.roll = r
+#         self.age = a 
+#         self.name = n
+
+#     def printdata(self):
+#         print(self.roll)
+#         print(self.age)
+#         print(self.name)
+
+# vaanya = Student()
+# vaanya.getData(102, 21, "vaanya")
+
+
+# vaanya.getData(a = 21,r = 102, n = "vaanya")
+
+# parth = Student()
+# parth.getData(103, 22, "parth")
+# vaanya.printdata()
+
+# print(vaanya.__doc__)
+# print(dir(vaanya))
+# ishika = Student()
+
+# print(type(ishika))
+# print(ishika)
+# 200
+# ishika.roll = 101
+# ishika.age = 21 
+# ishika.name = "ishika"
+
+# print(ishika.age)
+# "{}{{{{}}{{{{}}}}}}"
+# "{}{}{}}{{}{}"
