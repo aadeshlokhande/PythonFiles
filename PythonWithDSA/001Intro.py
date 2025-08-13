a4881f4cb692f21f7e14d3b43dfcc0e7844abbb5
Aadesh <aadeshlokhande11@gmail.com>
2025-08-13 19:57:47 +0530

    none

---
+++ PythonWithDSA/001Intro.py
@@ -0,0 +1,86 @@
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+# python language
+
+
+# binary ---> electronic device
+# high voltage ----> 1
+# low voltage -----> 0
+
+# hello ---> 1010101011111101000010111010101010110011111
+# code[1+2] ----> machine ----> 10101010100101010101[3]
+# 101001111[3] ---> machine -----> human readable[3]
+
+
+# machine ---> interpreter, compiler 
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+
+# print("Hello World!") 
+
+# print("hello,", end=" ")
+# print("Manthan")
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+# escape sequence
+# \n - new line
+# \t - horizontal tab
+# \\ = \
+# \a = alert
+# \b = backspace
+
+# print("Hello-\nbuddy")
+
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+# print("hello", "manthan", "asjhkdga","sddfgdfgs")
+# print(12,8,2025, sep="/")
+# 12/08/2025
+# 8:40:34
+
+# file01 = open("abc.txt","w")
+# print("hello buddy", file = file01)
+
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+# token ---> smallest element
+# print(10,20,40)
+
+# comments
+
+# maine yaha ek sentense print kiya hai
+# print("kaise ho bhai")
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+
+# escape sequence
+# \n - new line
+# \t - horizontal tab
+# \\ = \
+# \a = alert
+# \b = backspace
+# \' = '
+# \" = "
+
+# print("Bohmoshi nhi\n mitr...")
+# print("Bohmoshi nhi\t mitr...")
+
+# print("hello")
+# print("h\tello")
+# print("he\tllo")
+# print("hel\tlo")
+# print("hell\to")
+# print("hello\tpop")
+
+# print("hello \\nilesh")
+# print("ajhsdgjahgdaj \a hgdsajhg")
+
+# print("hello\b")
+
+# print("modi said \"mitro...\".")
+# print('this is my mom\'s mobile')
+
+
+# https://www.onlinegdb.com/
