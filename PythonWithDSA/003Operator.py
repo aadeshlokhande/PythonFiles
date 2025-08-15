bbcfbbcf87c56f016f0d3bac839ac953ef19787f
Aadesh <aadeshlokhande11@gmail.com>
2025-08-15 11:15:38 +0530

    none

---
+++ PythonWithDSA/003Operator.py
@@ -0,0 +1,114 @@
+# ████████████████████ OPERATOR ████████████████████
+
+# a = 10 
+# b = 20 
+# print(a + b)
+
+
+# arithmatic
+# unary
+# a = 10 
+# print(a)
+# print(-a) # -tive
+# print(+a) # +tive
+
+
+
+# binary
+# a = 5
+# b = 2 
+# print( a + b) #---> add 
+# print( a - b) #---> sub 
+# print( a * b) #---> multi 
+# print( a / b) #---> div = 2.5
+# print( a // b) #---> floor = 2
+# print( a ** b) #---> expo = 5 ** 3 ----> 5x5x5
+# print( a % b) #---> mod (remainder)
+
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+# logical
+# true ===> 1
+# false ===> 0
+# and ----> ziddi
+# chicken tandoori        biryani         feel
+    # 0                       0               0 
+    # 0                       1               0 
+    # 1                       0               0 
+    # 1                       1               1 
+
+# or ---> down to earth
+# chicken tandoori        biryani         feel
+        # 0                   0               0 
+        # 0                   1               1 
+        # 1                   0               1
+        # 1                   1               1
+
+
+# not ----> ulte khopdi
+# 1 ====> 0
+# 0 ====> 1 
+
+# not  true ===> false
+# not false ===> true
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+
+# relational
+
+# a = 10 
+# b = 20 
+
+# print("a < b ==> ",a < b)
+# print("a > b ==> ",a > b)
+# print("a == b ==> ",a == b)
+# print("a <= b ==> ",a <= b)
+# print("a >= b ==> ",a >= b)
+# print("a != b ==> ",a != b)
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+# assignment
+# a = 10
+# b = a
+
+# a = 10 
+# # a = 12
+# # a = a + 2
+# print(a)
+# a += 2 
+# print(a)
+# a -= 2 
+# print(a)
+# # a = a * 2
+# a *= 2 
+# print(a)
+# a /= 2 
+# print(a)
+# a %= 2 
+# print(a)
+
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+
+# ████████████████████ addition ████████████████████
+# num1 = int(input("enter a number = "))
+# num2 = int(input("enter a number = "))
+
+# print(num1, "+" , num2 , "=" ,num1+num2)
+
+
+# ⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛
+# ⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛ Homework ⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛
+# ⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛
+
+# sub
+# multi
+# div 
+# floor div 
+# mod 
+# expo 
+
+
+
