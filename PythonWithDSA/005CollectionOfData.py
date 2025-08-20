14164b1cf6f6f8ea20e6dc88c8f3b32c87cb97f9
Aadesh <aadeshlokhande11@gmail.com>
2025-08-20 21:11:03 +0530

    none

---
+++ PythonWithDSA/005CollectionOfData.py
@@ -0,0 +1,95 @@
+# ████████████████████ Collection Of Data  ████████████████████
+
+# var = 10
+# var = 10.7
+# var = "10.7"
+
+# ████████████████████ String ████████████████████
+
+# a s f h r f h r 
+# S G R D G 
+# 1 3 5 6 
+# % # % & $ @ 
+# "hello"
+
+# a = 'hello'
+# a = "hello"
+# a = "this is my mom's mobile"
+# a = 'modi said "hello mitro..."'
+# print(a)
+
+# a = '''hello asdasdd'''
+# a = """hello"""
+
+# a = str(190254)
+# print(a)
+# print(type(a))
+
+# ████████████████████ slicing ████████████████████
+# var = "awaj dur dur tak jani chahiye...."
+
+# var = "Aadesh Lokhande"
+
+# print(type(var))
+# print(var)
+# print(var[4])
+# print(var[-4])
+# print(var[1:4])
+# print(var[-6:-2])
+# print(var[1:13:2])
+# print(var[13:1:-2])
+# print(var[2:])
+# print(var[:13])
+# print(var[:])
+# print(var)
+# v = var[14::-1]
+# print(v)
+
+# ████████████████████ String Methods ████████████████████
+
+# pushpa = "dam hai toh rok ke dikha shakawat"
+
+# print(type(pushpa))
+# print(len(pushpa))
+# print(dir(pushpa))
+
+# print(pushpa.capitalize())
+# print(pushpa.upper())
+# print(pushpa.title())
+# print(pushpa.lower())
+# print(pushpa.swapcase())
+
+# print(pushpa.center(20,"█"))
+# print(pushpa.ljust(20,"█"))
+# print(pushpa.rjust(20,"█"))
+
+# print(pushpa.count("a"))
+# print(pushpa.index('m'))
+
+# print(pushpa.find("ai"))
+
+# print(pushpa.encode())
+# print(pushpa.isalnum())
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+# a = "123123pop "
+# utf-8
+# utf-16
+
+# import string
+
+# print(string.__doc__)
+# print(dir(string))
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+
+
+
+
+
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+# tuple 
+# list 
+# dictionary
+# set
