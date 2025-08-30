e3b05cb76442fa314f0cc81729ee7c1429b27f86
Aadesh <aadeshlokhande11@gmail.com>
2025-08-30 20:09:29 +0530

    none

---
+++ PythonWithDSA/006Functions.py
@@ -0,0 +1,95 @@
+# ████████████████████ Functions ████████████████████
+
+
+# def FunctionName(args):
+#     code
+
+
+
+# def greet():
+#     print("Hello user")
+# greet()
+
+# def greet(name):
+#     print(f"Hello {name}")
+
+# greet("aadesh")
+# greet("vivan")
+# greet("arjun")
+# greet("manthan")
+# greet("shantanu")
+
+
+
+# def add(a,b):
+#     print(a + b)
+
+# add(10,20)
+
+# print()
+# input()
+# int()
+# list()
+# tuple()
+# set()
+# dict()
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+# with argument with return
+
+# def add(a,b):
+#     c = a + b 
+#     return c
+
+# ans = add(10,20)
+# print(ans)
+# print(add(30,90))
+
+# with argument without return
+
+# def add(a,b):
+#     c = a + b 
+#     print(c)
+
+# a = add(10,20)
+
+# without argument with return
+# def add():
+#     a = 10
+#     b = 20
+
+#     c = a + b 
+#     return c 
+
+# value = add()
+# print(value)
+
+
+# without argument without return
+
+# def add():
+#     a = 10
+#     b = 20
+#     c = a + b 
+#     print(c)
+
+# add()
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+# def argument
+# *args
+# **kargs
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+# ⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛
+# ⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛ Homework ⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛
+# ⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛
+# sub 
+# multi 
+# div 
+# mod 
+# square
+# cube 
+
+# 24 programs
