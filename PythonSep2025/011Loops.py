22b81e88549415b8351850134ee76fef79b5ddee
Adesh <aadeshlokhande11@gmail.com>
2025-10-25 14:45:42 +0530

    WhileLoop

--- PythonSep2025/011Loops.py
+++ PythonSep2025/011Loops.py
@@ -39,7 +39,6 @@
 # print counting 10 to 1
 # for i in range(10,0,-1):
 # 	print(i)
-	
 
 # get number from user and print table 
 # num = 5
---
+++ PythonSep2025/013WhileLoop.py
@@ -0,0 +1,110 @@
+# ████████████████████ While Loop ████████████████████
+
+
+# while (condition):
+#     code 
+
+# a = 1 
+# while a<=10:
+#     print("Hello buddy",a)
+#     a= a+1
+
+# con = "yes"
+# while con=="yes":
+#     a = int(input("Ener a number = "))
+#     b = int(input("Ener a number = "))
+#     c = a+b 
+#     print(c)
+#     con = input("Do you want to continue = ")
+
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+# enter a -ve number = 4
+# enter a -ve number = 7
+# enter a -ve number = 5
+# enter a -ve number = 45
+# enter a -ve number = 65
+# enter a -ve number = 76
+# enter a -ve number = 56
+# enter a -ve number = -6
+# thanks for entering -ve number 
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+# var = 0
+# while var>=0:
+#     var = int(input("enter a -ve number = "))
+# print("thanks for entering -ve number")
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+# enter a number = 34
+# enter a number = 3
+# enter a number = 3
+# enter a number = 7
+# enter a number = 8
+# enter a number = 3
+# enter a number = 34
+# enter a number = 0 
+# total even numbers = 4 
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+# num = 2
+# count = 0
+# while num!=0:
+#     num=int(input("enter a number = "))
+#     if num%2==0:
+#         count += 1
+# print("total even number = ",count)
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+# num = 2
+# countEven = 0
+# countOdd = 0
+# while num!=0:
+#     num=int(input("enter a number = "))
+#     if num%2==0:
+#         countEven += 1
+#     else:
+#         countOdd+=1
+# print("total even number = ",countEven)
+# print("total odd number = ",countOdd)
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+# pass 
+# if ():
+#     pass
+
+
+# continue
+# for i in range(1,11):
+#     if i==5:
+#         continue
+#     print(i)
+
+
+# break 
+# for i in range(1,11):
+#     if i==5:
+#         break
+#     print(i)
+
+
+# for i in range(50):
+#     if i%2==1:
+#         print("--")
+#         continue
+#     print(i)
+
+
+
+
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+# ⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛
+# ⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛ Homework ⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛
+# ⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛
+
