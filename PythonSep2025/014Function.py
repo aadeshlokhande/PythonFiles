114526f8e5d8526ad8ce88f9004a8acb61040096
Adesh <aadeshlokhande11@gmail.com>
2025-10-28 21:05:43 +0530

    None

---
+++ PythonSep2025/014Function.py
@@ -0,0 +1,117 @@
+# ████████████████████ Functions ████████████████████
+
+# build-in  ---> pre-define
+# int()
+# print()
+# input()
+# str()
+# list()
+# tuple()
+# dic()
+# set()
+
+
+# user define
+
+# def FunctionName(argument):
+#     code 
+#     return value 
+
+# FunctionName(values)
+
+# example
+# def greet():
+#     print("Hello buddy... Good morning")
+
+# if
+#     greet()
+# 50 line 
+# greet 
+# 100 lines 
+# greet 
+
+# greet()
+# greet()
+# greet()
+# greet()
+
+
+
+
+# def greet(name):
+#     print(f"Hello {name}... Good morning")
+
+# greet("khushi")
+# greet("aadesh")
+
+
+
+# def greet(name,age):
+#     print(f"Hello {name}... my age is {age}")
+
+# greet("khushi",90)
+# greet("aadesh",74)
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+# with argument with return 
+
+# def add(a,b):
+#     c = a + b 
+#     return c
+
+# ans = add(2,7)
+# print("answer = ",ans)
+
+# print("answer = ",add(10,20))
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+# with argument without return 
+
+# def add(a,b):
+#     c = a + b 
+#     print(f"{a} + {b} = {c}")
+
+# add(2,7)
+# add(20,70)
+
+
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+# without argument with return 
+
+# def add():
+#     a = int(input("Enter a number = "))
+#     b = int(input("Enter a number = "))
+#     c = a + b 
+#     return c 
+
+
+# a = add()
+# print(a)
+# print(add())
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+# without argument without return
+
+# def add():
+#     a = int(input("num = "))
+#     b = int(input("num = "))
+#     v = a + b 
+#     print(v)
+# add()
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+
+# ⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛
+# ⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛ Homework ⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛
+# ⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛⊛
+# 24 function 
+
+# sub 
+# multi 
+# div 
+# mod 
+# sqaure
+# cube 
+
+
