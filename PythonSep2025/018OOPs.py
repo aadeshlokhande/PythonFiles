5b0a23a28a1ace6bfbf1c0b530aa933040ad97d6
Adesh <aadeshlokhande11@gmail.com>
2025-11-06 20:40:54 +0530

    msg

---
+++ PythonSep2025/018OOPs.py
@@ -0,0 +1,48 @@
+# ████████████████████ OOPs ████████████████████
+
+# OOPS
+# class 
+# object 
+# inheritance
+# polymorphism
+# abstraction
+# encapsulation
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+# ████████████████████ Class and Object ████████████████████
+
+
+# class Student:
+#     pass 
+
+# khushi = Student()
+# print(type(khushi))
+# khushi.name = "khushi"
+# khushi.age =20
+# khushi.address = "nagpur"
+
+# print(khushi.age)
+
+# ████████████████████████████████████████
+
+class Student:
+    def info(self,name1, age1, address1):
+        self.name = name1
+        self.age = age1 
+        self.address = address1
+
+    def printData(self):
+        print(self.name)
+        print(self.age)
+        print(self.address)
+
+khushi = Student()
+khushi.info("Khushi", 20, "Nagpur")
+
+# print(khushi.age,"years")
+# khushi.printData()
+pratik = Student()
+pratik.info("Pratik", 19, "Goa")
+
+
+pratik.printData()
