55c37a1b212c3e6319bd85e86c3537b24c707465
Adesh <aadeshlokhande11@gmail.com>
2025-11-13 20:45:08 +0530

    msg

---
+++ PythonSep2025/020inheritance.py
@@ -0,0 +1,51 @@
+# ████████████████████ Inheritance ████████████████████
+
+# parent class ---> base class 
+# child class ---> derive class
+
+# ████████████████████  single  ████████████████████
+# class Parent:
+#     def ParentQuality(self):
+#         print("paisa kamana")
+    
+# class Child(Parent):
+#     def ChildQuality(self):
+#         print("Masti Krna")
+
+# jethalal = Parent()
+# jethalal.ParentQuality()
+
+# tappu = Child()
+# tappu.ChildQuality()
+# tappu.ParentQuality()
+
+# employee ---> name, age, roll, salary, leaves = 12, id, ......100 chije
+# programmer ---> leaves = 15
+
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+# ████████████████████ multiple ████████████████████
+
+# class Daddy:
+#     def DadQuality(self):
+#         print("cricket")
+
+# class Mummy:
+#     def MomQuality(self):
+#         print("Cooking")
+
+# class Child(Daddy, Mummy):
+#     def Childquality(self):
+#         print("Dancing")
+
+# Khushi = Child()
+# Khushi.Childquality()
+# Khushi.MomQuality()
+# Khushi.DadQuality()
+
+
+
+# ╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩╦╩
+# multilevel
+# heirarchical
+# hybrid
