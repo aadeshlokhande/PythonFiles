1063aa517e630ae4272ee153c47f58c6c358e82b
Adesh <aadeshlokhande11@gmail.com>
2025-12-03 20:41:39 +0530

    msg

--- PythonSep2025/027Pywhatkit.py
+++ PythonSep2025/027Pywhatkit.py
@@ -5,9 +5,5 @@
 # pk.playonyt("shinchan song dj")
 # pk.search("Tajmahal")
 # pk.info("amitabh bachan")
-
 # pk.sendwhatmsg("919999999999", message="hello")
-
 # pk.image_to_ascii_art("a.jpg", "a.txt")
-
-
